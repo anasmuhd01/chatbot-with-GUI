042b103bdc4f1f19eb4cb18e0b632f024ec11a86
anas <ansmuhd01@gmail.com>
2023-10-29 13:02:28 +0530

    initial commit, GUI created

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton
+
+
+class ChatbotWIndow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(700, 500)
+
+        self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10, 10, 480, 320)
+        self.chat_area.setReadOnly(True)
+
+        self.input_field = QLineEdit(self)
+        self.input_field.setGeometry(10, 340, 480, 40)
+
+        self.button = QPushButton("send", self)
+        self.button.setGeometry(500, 340, 100, 40)
+
+        self.show()
+
+
+app = QApplication(sys.argv)
+main_window = ChatbotWIndow()
+sys.exit(app.exec())
+
+
